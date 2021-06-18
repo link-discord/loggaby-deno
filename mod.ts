b21d642c73dd6d21ea94441cddb0631c641a360b
Link <50463727+LINKdiscordd@users.noreply.github.com>
2021-06-18 14:43:28 +0200

    publish

---
+++ mod.ts
@@ -0,0 +1,64 @@
+import * as Colors from 'https://deno.land/std@0.99.0/fmt/colors.ts'
+
+interface previousLog {
+	type: string
+	message: string
+}
+
+export default class Logger {
+	previousLog: previousLog
+	spacing: boolean
+
+	constructor(spaceBetweenTags = true) {
+		this.previousLog = {
+			type: 'none',
+			message: ''
+		}
+
+		this.spacing = spaceBetweenTags
+	}
+
+	success(data: unknown, customTag?: string) {
+		const tag = customTag ?? 'Success'
+
+		if (this.spacing && this.previousLog.type !== tag.toLowerCase()) console.log('')
+
+		const displayTag = Colors.bgBrightGreen(Colors.black(` ${tag} `))
+		console.log(`${displayTag} ${data}`)
+
+		this.previousLog = { message: String(data), type: tag.toLowerCase() }
+	}
+
+	log(data: unknown, customTag?: string) {
+		const tag = customTag ?? 'Log'
+
+		if (this.spacing && this.previousLog.type !== tag.toLowerCase()) console.log('')
+
+		const displayTag = Colors.bgBrightCyan(Colors.black(` ${tag} `))
+		console.log(`${displayTag} ${data}`)
+
+		this.previousLog = { message: String(data), type: tag.toLowerCase() }
+	}
+
+	error(data: unknown, customTag?: string) {
+		const tag = customTag ?? 'Error'
+
+		if (this.spacing && this.previousLog.type !== tag.toLowerCase()) console.log('')
+
+		const displayTag = Colors.bgBrightRed(Colors.black(` ${tag} `))
+		console.log(`${displayTag} ${data}`)
+
+		this.previousLog = { message: String(data), type: tag.toLowerCase() }
+	}
+
+	warn(data: unknown, customTag?: string) {
+		const tag = customTag ?? 'Warning'
+
+		if (this.spacing && this.previousLog.type !== tag.toLowerCase()) console.log('')
+
+		const displayTag = Colors.bgBrightYellow(Colors.black(` ${tag} `))
+		console.log(`${displayTag} ${data}`)
+
+		this.previousLog = { message: String(data), type: tag.toLowerCase() }
+	}
+}
--- test.ts
+++ test.ts
@@ -1,9 +1,4 @@
-# loggaby-deno
- A logger package inspired by loggaby for deno
-
-## Usage
-```ts
-import Logger from 'https://deno.land/x/loggaby/mod.ts'
+import Logger from './mod.ts'
 
 // You can add a boolean as an option to have log messages grouped together
 const logger = new Logger(true)
@@ -19,7 +14,5 @@ logger.log('This is a log message with a custom prefix', 'Custom')
 logger.warn('This is a Warning message')
 logger.error('This is a Error message')
 logger.success('This is a Success message')
-```
 
-### Result
-![Image of console](https://cdn.discordapp.com/attachments/813818056932589568/855188855892213770/unknown.png)
+console.log('')
